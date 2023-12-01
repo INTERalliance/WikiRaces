39aba1823b8b485369ba8c68bf32f9a5d12b1bbd
CG <congodsted@protonmail.com>
2023-12-01 00:02:03 -0500

    QOL features for level generation
    Cleaner text messages
    Case insensitive accept message
    Added comments to Python script
    Additional confirmation of how many levels have been generated before saving.

--- src/admin_tools/generateLevels.py
+++ src/admin_tools/generateLevels.py
@@ -32,7 +32,7 @@ i = 0
 def get_name() -> str:
     global i
     i += 1
-    print(f"Using 'level{i}'")
+    print(f"Now editing level {i}.")
     return f"level{i}"
 
 
@@ -45,28 +45,34 @@ def end_time() -> str:
     print("Enter the time the level should close. (e.g. 'in 10 min', 'Fri, 12 Dec 2023 10:59:00')")
     return get_time()
 
-
+# This function accepts a new level from user input.
 def new_page(point):
     proceed = True
     while proceed != False:
+        # ask for start/end page
         if point == "start":
             print("Enter the part of the wikipedia url after 'https://en.wikipedia.org/wiki/' for the starting page.")
         else:
             print("Enter the part of the wikipedia url after 'https://en.wikipedia.org/wiki/' for the ending page.")
         
         print("Or, press enter to randomize from the list of levels.")
+        # read result
         result = input(cursor)
+        # randomly generate a result if not specify
         if result == "":
             result = fromList()
+        # verify that page exists
         page = wiki.page(result)
         proceed = not page.exists()
+    # confirm result
+    print("The " + point + " of level " + str(i) + " will be " + result)
     return result
 
+# This function reads from a file of known good levels, to allow for automatically generated level data.
 def fromList():
     with open(os.path.join(os.pardir, "game/game_static/knowngood.csv"), 'r') as file: 
         for row in csv.reader(file):
             result = random.choice(row)
-        print(result)
         return result
 
 def generate_level():
@@ -87,6 +93,7 @@ def add_level():
 def accept(prompt, repeat=True):
     print(prompt)
     choice = input("> ")
+    choice = choice.lower()[0]
     if choice == "y":
         return True
     elif choice == "n":
@@ -95,10 +102,12 @@ def accept(prompt, repeat=True):
         return accept("Please specify (y/n)")
 
 
-while accept("Would you like to add another level? (y/n)"):
+while accept("Would you like to add another level?"):
     add_level()
 
-if accept("Would you like to save these levels? (y/n)"):
+saveMessage = "Would you like to save " + str(len(levels)) + " level(s)?"
+
+if accept(saveMessage):
     print("Written and backed up")
     backup()
     set_json(json.dumps(levels, indent=4))

75ae3e35a095f42e6bd564ace9dc22d506790870
Zack Sargent <zackarywsargent@gmail.com>
2023-12-14 11:28:58 -0500

    fails to connect to server when using localhost for some reason

--- src/game/database.js
+++ src/game/database.js
@@ -2,7 +2,7 @@
 
 // mongoclient, to connect to mongodb
 var MongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017/";
+var url = "mongodb://127.0.0.1:27017/";
 
 // bunyan, for logging
 const bunyan = require("bunyan");

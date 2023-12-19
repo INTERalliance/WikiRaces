c75e1313c4c927261b293e550c8c8815000a7152
Zack Sargent <zack@zack.fyi>
2023-12-19 14:54:26 -0500

    migrate from regex based filtering to css filtering

--- src/game/dynamic.js
+++ src/game/dynamic.js
@@ -31,38 +31,12 @@ const { fillTemplate } = require("./template");
 const wiki = require('wikipedia');
 
 /**
- * Queries the Wikipedia Page API for a given page ID.
- *
- * This currently uses the 
- * [Wikipedia npm library](https://github.com/dopecodez/Wikipedia)
- * which should call the 
- * [GET `/page/html/{title}` endpoint](https://en.wikipedia.org/api/rest_v1/#/Page%20content/get_page_html__title_).
- * @param {string} id 
+ * Takes HTML and processes it to be embedded in the template by
+ * removing html and body tags.
+ * @param {string} page - a raw HTML page returned from wikipedia
  * @returns {string}
  */
-async function getWiki(id) {
-	try {
-		log.info(`Downloading ${id}`);
-		const page = await wiki.page(id);
-		const html = await page.html();
-		return html;
-	} catch (error) {
-		log.warn(error);
-		return undefined;
-	}
-}
-
-function tryRemoveClass(page, name) {
-	const regex = new RegExp(`<div.*class="${name}.*>([\s\S]*?)<\/div>`, "gm");
-	return page.replace(regex, "");
-}
-
-function tryRemoveId(page, name) {
-	const regex = new RegExp(`<div.*id="${name}.*>([\s\S]*?)<\/div>`, "gm");
-	return page.replace(regex, "");
-}
-
-async function formatPage(page) {
+function processHTML(page) {
 	// I previously used a DOM emulator (jsdom) to remove elements,
 	// and, in terms of performance, this wall of regex is significantly better.
 
@@ -77,56 +51,36 @@ async function formatPage(page) {
 		/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
 		""
 	);
-	// remove footer
-	page = page.replace(/<footer[\s\S]*<\/footer>/gm, "");
-	// remove citations
-	page = page.replace(/<h2.*id="References.*h2>/gm, "");
-	page = page.replace(/<li id="cite_note([\s\S]*?)<\/li>/gm, "");
-	// remove navigation
-	page = page.replace("<h2>Navigation menu</h2>", "");
-	page = page.replace(
-		/<div id="mw-navigation([\s\S]*?)<\/form>\n<\/div>/gm,
-		""
-	);
-	// remove external links section
-	page = page.replace(/<nav id="p-([\s\S]*?)<\/nav>/gm, "");
-	page = page.replace(/<h2.*External links.*<\/h2>/gm, "");
-	page = page.replace(/<table.*sistersitebox([\s\S]*?)<\/table>/gm, "");
-	page = page.replace(/<div role.*sistersitebox.*$/gm, "");
-	// replace external links with text
-	page = page.replace(/<a .*external text.*<\/a>/g, "[external link]");
-	// remove Jump To ___ links
-	page = page.replace(/<a class.*Jump.*/g, "");
-	// remove php scripts
-	page = page.replace(/<link rel.*php\?.*/g, "");
-	// remove all [edit] boxes without removing the end tags
-	page = page.replace(/<span class="mw-editsection">.*(?=(<\/h.*>))/g, "");
-	page = page.replace(/<div.*class="refbegin.*>([\s\S]*?)<\/div>/gm, "");
-	page = page.replace(
-		/<div role="navigation".*navbox auth.*>([\s\S]*?)<\/div>/gm,
-		""
-	);
-	// remove divs by class
-	page = tryRemoveClass(page, "reflist");
-	page = tryRemoveClass(page, "printfooter");
-	page = tryRemoveClass(page, "navbox authority-control");
-	page = tryRemoveClass(page, "mw-indicator");
-	// remove divs by id
-	page = tryRemoveId(page, "catlinks");
-	page = tryRemoveId(page, "p-search");
 
 	return page;
 }
 
+/**
+ * Queries the Wikipedia Page API for a given page ID.
+ *
+ * This currently uses the 
+ * [Wikipedia npm library](https://github.com/dopecodez/Wikipedia)
+ * which should call the 
+ * [GET `/page/html/{title}` endpoint](https://en.wikipedia.org/api/rest_v1/#/Page%20content/get_page_html__title_).
+ * @param {string} id 
+ * @returns {Promise<string>}
+ */
 async function generatePage(id) {
 	// get raw html from wikipedia.
-	let page = await getWiki(id);
-	if (!page) {
-		return "This page does not exist.";
+	try {
+		log.info(`Downloading ${id}`);
+		let page = await wiki.page(id);
+		if (!page) {
+			return "This page does not exist.";
+		}
+		let html = await page.html();
+		html = processHTML(html);
+		page = fillTemplate(html, id, page.title);
+		return page;
+	} catch (error) {
+		log.warn(error);
+		return undefined;
 	}
-	page = await formatPage(page);
-	page = await fillTemplate(page, id);
-	return page;
 }
 
 // make folder if none exists
@@ -141,7 +95,7 @@ try {
 
 /**
  * save file to cache
- * @param {string} id - A [URI encoded](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent) Wikipedia Page ID
+ * @param {string} id - A [URI encoded](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent) Wikipedia Page ID or filename
  * @param {string | NodeJS.ArrayBufferView} content - the content to write to the file
  * @param {string?} suffix - any file extension for the file name / id. defaults to ".html"
  */
@@ -159,6 +113,11 @@ async function saveFile(id, content, suffix = ".html") {
 	}
 }
 
+/**
+ * checks if Wikipedia Page Id is cached locally. 'id' should not include the file extension, but should be URI encoded
+ * @param {string} id - A [URI encoded](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent) Wikipedia Page ID 
+ * @returns {boolean}
+ */
 function isCached(id) {
 	try {
 		return fs.existsSync(`${cacheFolder}/${id}.html`);
@@ -183,24 +142,25 @@ async function getCached(id, suffix = ".html") {
 	}
 }
 
-// gets file from cache if it exists,
-// otherwise it downloads the file from the internet.
+/**
+ * Gets file from cache if it exists, otherwise it downloads the file from the internet.
+ * @param {string} id - Wikipedia page id (NOT URI encoded)
+ * @returns {string} the entire html of the Wikipedia page, processed for embedding
+ */
 async function getPage(id) {
-	// const encodedId = encodeURIComponent(id);
+	const encodedId = encodeURIComponent(id);
 	try {
-		//let page = "";
-		let page = await generatePage(id);
-		// TODO: UNDO THIS
-		// if (isCached(encodedId)) {
-		// 	avg.add(1);
-		// 	page = await getCached(encodedId);
-		// }
-		// if (!page) {
-		// 	avg.add(0);
-		// 	page = await generatePage(id);
-		// 	saveFile(encodedId, page);
-		// }
-		// log.info(`${Number(((await avg.average()) * 100).toFixed(4))}% cached`);
+		let page = "";
+		if (isCached(encodedId)) {
+			avg.add(1);
+			page = await getCached(encodedId);
+		}
+		if (!page) {
+			avg.add(0);
+			page = await generatePage(id);
+			saveFile(encodedId, page);
+		}
+		log.info(`${Number(((await avg.average()) * 100).toFixed(4))}% cached`);
 		return page;
 	} catch (err) {
 		log.error(`Error in page load ${err}`);
@@ -210,7 +170,5 @@ async function getPage(id) {
 module.exports = {
 	getPage,
 	getCached,
-	getWiki,
 	saveFile,
-	getCached,
 };

1454e6380eed9d134c59c98898fe87d1f6a86e28
Cassian Godsted <cassian@godsted.com>
2025-01-24 13:29:51 -0500

    i love css

--- src/game/template.js
+++ src/game/template.js
@@ -27,6 +27,9 @@ function fillTemplate(content, id, pageTitle) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="../wiki.css">
     <link rel="stylesheet" href="../wiki-custom.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 
 <body class="outer-wrapper">
